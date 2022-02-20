ed9e4dd3b6a0e7ae42fa79a4ba613363e15cbb31
akhmadullinaynur <akhmadullinaynur@yandex.ru>
2022-02-20 16:06:33 +0300

    HW Cart

--- ITParkMongoDB/Client.cs
+++ ITParkMongoDB/Client.cs
@@ -8,16 +8,64 @@ namespace ITParkMongoDB
 {
     class Client
     {
-        public Client(string nameOfProduct, double cash, double countOfProduct)
+        [BsonId]
+        [BsonIgnoreIfDefault]
+        ObjectId _id;
+        public string Name { get; set; }
+
+        [BsonElement("Cart")]
+        public Cart clientsCart = new Cart();
+
+        public Client(string name)
         {
-            NameOfProduct = nameOfProduct;
-            Cash = cash;
-            CountOfProduct = countOfProduct;
+            Name = name;
         }
 
-        public string NameOfProduct { get; set; }
-        public double Cash { get; set; }
-        public double CountOfProduct { get; set; }
+        public void Buy(string name, string category, double count)
+        {
+            var list = DataBaseMethods.ShowProductsInCategory(category);
+            if (list.Exists(x => x.NameOfProduct == name))
+            {
+                var current = list.Find(x => x.NameOfProduct == name);
+                if (current.CountAtWarehouse >= count)
+                {
+                    current.CountAtWarehouse -= count;
+                    DataBaseMethods.ReplaceProduct(current);
+                    current.CountAtWarehouse = count;
+                    clientsCart.AddToCart(current);
+                    Console.WriteLine(current.NameOfProduct + " " + current.Manufacturer + " " + current.CountAtWarehouse);
+                }
+                else
+                {
+                    Console.WriteLine("Not enough products in warehouse");
+                }
+                DataBaseMethods.ReplaceProduct(current);
+            }
+            else
+                Console.WriteLine("Not found");
+        }
 
+        public void ShowCart()
+        {
+            foreach (var item in clientsCart.cart)
+            {
+                Console.WriteLine($"{item.NameOfProduct} {item.CountAtWarehouse}");
+            }
+
+            Console.WriteLine("Total cost is: " + clientsCart.Currency);
+        }
+
+        public static void ClientsLog(Client client)
+        {
+            var list = DataBaseMethods.FindClient(client.Name);
+            if (list.Exists(x => x.Name == client.Name))
+            {
+                DataBaseMethods.ReplaceClientToDatabase(client);
+            }
+            else
+            {
+                DataBaseMethods.AddClientToDatabase(client);
+            }
+        }
     }
 }
--- ITParkMongoDB/DataBaseMethods.cs
+++ ITParkMongoDB/DataBaseMethods.cs
@@ -62,26 +62,52 @@ namespace ITParkMongoDB
             var collection = database.GetCollection<Administrator>("Administrator");
             collection.ReplaceOne(x => x.name == nameToReplace , newInfo);
         }
-        public static List<Product> Shopping(string shopping, Product newCount)
+        public static void ReplaceProduct(Product product)
         {
             var client = new MongoClient("mongodb://localhost");
             var database = client.GetDatabase("Magnit");
-            var collection = database.GetCollection<Product>("Bakery");
-            collection.ReplaceOne(x => x.NameOfProduct == shopping, newCount);
-            return collection.Find(x => true).ToList();
-
-            //if (list.Count != 0)
-            //{
-            //    System.Console.Write("Shoping list:");
-            //    foreach (var item in list)
-            //    {
-            //        System.Console.WriteLine($"{item.NameOfProduct} {item.Price}{item.CountAtWarehouse}");
-            //    }
-            //    newCount.CountAtWarehouse -= newCount.CountAtWarehouse;
-            //}
-            //else
-            //    System.Console.WriteLine("Current product not exist in data base");
+            var collection = database.GetCollection<Product>(product.TypeOfProduct);
+            collection.ReplaceOne(x => x.NameOfProduct == product.NameOfProduct, product);
         }
+
+        public static List<string> GetCollections()
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Magnit");
+            return database.ListCollectionNames().ToList();
+        }
+        public static void AddClientToDatabase(Client client1)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Magnit");
+            var collection = database.GetCollection<Client>("Logs");
+            collection.InsertOne(client1);
+        }
+
+        public static void ReplaceClientToDatabase(Client client1)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Magnit");
+            var collection = database.GetCollection<Client>("Logs");
+            collection.ReplaceOne(x => x.Name == client1.Name, client1);
+        }
+        public static List<Client> FindClient(string name)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Magnit");
+            var collection = database.GetCollection<Client>("Logs");
+            return collection.Find(x => x.Name == name).ToList();
+        }
+
+        public static Cart GetCart(string name)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Magnit");
+            var collection = database.GetCollection<Client>("Logs");
+            var client1 = collection.Find(x => x.Name == name).FirstOrDefault();
+            return client1.clientsCart;
+        }
+    }
 
     }
 }
